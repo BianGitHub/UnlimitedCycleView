5319a62660e79ad86d9081de95d823ea2a6fc4df
边雷 <775884216@qq.com>
2016-12-31 14:47:12 +0800

    创建控制器,导入素材

---
+++ UnlimitedCycleView/UnlimitedCycleView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UnlimitedCycleView
+//
+//  Created by 边雷 on 16/12/31.
+//  Copyright © 2016年 Mac-b. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
